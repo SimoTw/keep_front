58e3f91e46f0446aff613dfbccb201c9bb9f1982
SimoTW <simohsiehtw@gmail.com>
2019-12-27 20:50:28 +0800

    better env set

--- src/api/helpers/baseURL.js
+++ src/api/helpers/baseURL.js
@@ -1,3 +1,12 @@
-export const baseURL = "http://keep-21.us-west-2.elasticbeanstalk.com/api";
-
-// export const baseURL = "http://localhost:3000/api";
+export const baseURL = (function() {
+  switch (process.env.NODE_ENV) {
+    case "development": {
+      return "http://localhost:3000/api";
+    }
+    case "production": {
+      return "http://keep-21.us-west-2.elasticbeanstalk.com/api";
+    }
+    default:
+      throw new Error(`unhandleable NODE_ENV ${process.env.NODE_END}`);
+  }
+})();
