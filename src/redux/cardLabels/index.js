416029590667cb657ca818b806f5602bd99ec3c1
simo <simohsiehtw@gmail.com>
2020-11-28 13:42:32 +0800

    add card reducer actions

--- src/redux/cardLabels/index.js
+++ src/redux/cardLabels/index.js
@@ -1,10 +1,10 @@
-const initState = { byIds: {}, allIds: [] };
+const initState = { byIds: {}, allIds: [] }
 
 const types = {
   add: "add",
   remove: "remove",
   set: "set",
-};
+}
 
 /**
  * this is for many to many relations for cards and labels
@@ -12,8 +12,8 @@ const types = {
 function cardLabelReducer(state = initState, action) {
   switch (action.type) {
     case types.add: {
-      const { cardId, labelId } = action.payload;
-      const id = state.allIds[state.addIds.length - 1] + 1 || 0;
+      const { cardId, labelId } = action.payload
+      const id = state.allIds[state.addIds.length - 1] + 1 || 0
       return {
         byIds: {
           ...state.byIds,
@@ -24,24 +24,44 @@ function cardLabelReducer(state = initState, action) {
           },
         },
         allIds: [...state.allIds, id],
-      };
+      }
     }
     case types.remove: {
-      const { id } = action;
-      const nextByIds = { ...state.byIds };
-      delete nextByIds[id];
-      const nextAllIds = [...state.addIds];
-      nextAllIds.splice(id, 1);
+      const { id } = action
+      const nextByIds = { ...state.byIds }
+      delete nextByIds[id]
+      const nextAllIds = [...state.addIds]
+      nextAllIds.splice(id, 1)
       return {
         byIds: nextByIds,
         allIds: nextAllIds,
-      };
+      }
     }
     case types.set: {
-      const { labels } = action.payload;
-      return labels;
+      const { labels } = action.payload
+      return labels
     }
     default:
-      return state;
+      return state
   }
 }
+
+const add = (cardId, labelId) => ({
+  type: types.add,
+  payload: {
+    cardId,
+    labelId,
+  },
+})
+
+const remove = (id) => ({
+  type: types.remove,
+  id,
+})
+
+const set = (labels) => ({
+  type: types.set,
+  payload: {
+    labels,
+  },
+})
